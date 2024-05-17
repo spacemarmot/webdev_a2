33a0d3fd7c79106036ae44f8f7e7693c08014b0d
DaggerMatt <55683697+DagegerMatt-cloud@users.noreply.github.com>
2024-05-18 02:12:35 +1200

    404 revamp
    - changed surface

--- js/function.js
+++ js/function.js
@@ -5,3 +5,17 @@
     $(document).ready(function(){
         $('[data-toggle="tooltip"]').tooltip(); 
     });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const hoverImage = document.getElementById('wide');
+        const audio = document.getElementById('audio');
+    
+        hoverImage.addEventListener('mouseenter', function() {
+      
+            audio.play();
+        });
+    
+        hoverImage.addEventListener('mouseleave', function() {
+            audio.pause();
+        });
+    });
