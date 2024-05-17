5d767716fc627bc79a784378e15d6d954e7d10a9
DaggerMatt <55683697+DagegerMatt-cloud@users.noreply.github.com>
2024-05-18 03:46:24 +1200

    doot doot
    :100: :100:  :100: :expressionless: :anchor: :airplane:

--- js/function.js
+++ js/function.js
@@ -10,12 +10,19 @@
         const hoverImage = document.getElementById('wide');
         const audio = document.getElementById('audio');
 		audio.volume = 0.1;    
+        
         hoverImage.addEventListener('mouseenter', function() {
-      
+            
             audio.play();
+
         });
     	
         hoverImage.addEventListener('mouseleave', function() {
             audio.pause();
         });
     });
+
+    function Skele() {
+    var skeleRat = document.getElementById("skeleRat");
+    skeleRat.removeAttribute("hidden"); // Show the image
+}
